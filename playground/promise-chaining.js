80a274b7639e6b99b16021bec3d32f831ed134b7
jay patel <jaypatel@simformsolutions.com>
2022-02-02 11:58:34 +0530

    added promise chaining demo II

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,16 +2,26 @@ require('../src/db/mongoose')
 const User = require("../src/models/user")
 const Task = require("../src/models/task")
 
-//61fa0f5874dae7f086db5239
+// //61fa0f5874dae7f086db5239
+// User.findByIdAndUpdate('61fa0f5874dae7f086db5239',{age:1})
+// .then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({age:1})
+// })
+// .then((count)=>{
+//     console.log(count)
+// })
+// .catch((e)=>{
+//     console.log(e)
+// })
 
-User.findByIdAndUpdate('61fa0f5874dae7f086db5239',{age:1})
-.then((user)=>{
-    console.log(user)
-    return User.countDocuments({age:1})
+//61fa0d531c299e0729780309
+
+Task.findByIdAndDelete('61fa0d531c299e0729780309')
+.then((task)=>{
+    console.log(task)
+    return Task.countDocuments({completed:true})
 })
 .then((count)=>{
     console.log(count)
-})
-.catch((e)=>{
-    console.log(e)
 })
